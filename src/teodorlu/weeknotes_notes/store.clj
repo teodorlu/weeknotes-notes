a9a957949cb1e3f24b85060765f2d135c2757531
Teodor Heggelund <git@teod.eu>
2024-04-15 19:15:54 +0200

    a data store

---
+++ src/teodorlu/weeknotes_notes/store.clj
@@ -0,0 +1,30 @@
+(ns teodorlu.weeknotes-notes.store
+  (:require
+   [babashka.fs :as fs]
+   [clojure.edn :as edn]
+   [clojure.string :as str]))
+
+;; a storage protocol for maps with a :uuid key
+
+(defprotocol Store
+  (save-one [store data])
+  (list-uuids [store])
+  (load-one [store uuid]))
+
+(defrecord FolderBackedEdnStore [root]
+  Store
+  (save-one [_store data]
+    (let [path (fs/file root (str (:uuid data) ".edn"))]
+      (spit path (pr-str data))))
+
+  (list-uuids [_store]
+    (->> (fs/glob root "*.edn")
+         (map fs/file-name)
+         (map #(str/replace % #"\.edn$" ""))
+         (remove str/blank?)
+         (map parse-uuid)))
+
+  (load-one [_store uuid]
+    (let [path (fs/file root (str uuid ".edn"))]
+      (when (fs/exists? path)
+        (edn/read-string (slurp path))))))
