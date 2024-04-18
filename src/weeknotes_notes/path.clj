92bfb5d6055f41a0270cf652bd45873e26816482
Teodor Heggelund <git@teod.eu>
2024-04-18 09:44:47 +0200

    Duplicate everything

---
+++ src/weeknotes_notes/path.clj
@@ -0,0 +1,8 @@
+(ns weeknotes-notes.path)
+
+(def root "/")
+(def submit-note "/submit-note")
+(def submit-note-note-text-name "note-text")
+
+(def github-shortname "teodorlu/weeknotes-notes")
+(def github-source-url "https://github.com/teodorlu/weeknotes-notes")
