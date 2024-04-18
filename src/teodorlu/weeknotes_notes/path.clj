588c4c83a6199e515a8cab9e1953b47f803d3f4b
Teodor Heggelund <git@teod.eu>
2024-04-18 09:47:32 +0200

    Delete the old stuff

--- src/teodorlu/weeknotes_notes/path.clj
+++
@@ -1,9 +0,0 @@
-(ns teodorlu.weeknotes-notes.path
-    {:deprecated true})
-
-(def root "/")
-(def submit-note "/submit-note")
-(def submit-note-note-text-name "note-text")
-
-(def github-shortname "teodorlu/weeknotes-notes")
-(def github-source-url "https://github.com/teodorlu/weeknotes-notes")
