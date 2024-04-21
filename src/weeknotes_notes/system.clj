7dc848a090cfa5104db5898e659f3aafc554a2fc
Teodor Heggelund <git@teod.eu>
2024-04-21 09:59:51 +0200

    Fix: old path was wrong!

--- src/weeknotes_notes/system.clj
+++ src/weeknotes_notes/system.clj
@@ -1,7 +1,8 @@
 (ns weeknotes-notes.system
   (:require
+   [clojure.string :as str]
    [integrant.core :as ig]
-   [clojure.string :as str]))
+   [weeknotes-notes.store :as store]))
 
 ;; store
 ;;
@@ -14,15 +15,33 @@
 :weeknotes.notes/http-server
 
 (defn config-dev []
-  {:weeknotes-notes/store {:root ".local/storage"}
+  {:weeknotes-notes/store {:root ".local/storage/edn-store"}
    :weeknotes-notes/injected-app {}
    :weeknotes.notes/http-server {:port 7984}})
 
 (defn config-prod []
   (assert (not (str/blank? (System/getenv "GARDEN_STORAGE"))))
-  {:weeknotes-notes/store {:root (System/getenv "GARDEN_STORAGE")}
+  {:weeknotes-notes/store {:root (str (System/getenv "GARDEN_STORAGE")
+                                      "/edn-store")}
    :weeknotes-notes/injected-app {}
    :weeknotes.notes/http-server {:port 7777}})
 
-(defmethod ig/init-key :weeknotes/store
-  [_ _])
+(defmethod ig/init-key :weeknotes-notes/store
+  [_ {:keys [root]}]
+  (store/->FolderBackedEdnStore root))
+
+(defmethod ig/init-key :weeknotes-notes/injected-app
+  [_ _]
+  "injected app")
+
+(defmethod ig/init-key :weeknotes-notes/http-server
+  [_ _]
+  "http server")
+
+(comment
+  (def mysys (ig/init (config-dev)))
+  (ig/halt! mysys)
+
+  (store/list-uuids (:weeknotes-notes/store mysys))
+
+  )
