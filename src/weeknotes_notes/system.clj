6a3d3218a7f39877f0b7ddc96eb686ac5f41c25e
Teodor Heggelund <git@teod.eu>
2024-04-21 09:51:17 +0200

    system design - system components, relations

--- src/weeknotes_notes/system.clj
+++ src/weeknotes_notes/system.clj
@@ -3,20 +3,26 @@
    [integrant.core :as ig]
    [clojure.string :as str]))
 
-;; injected app
-;;
 ;; store
+;;
+;; injected app
 ;;
 ;; http-server
 
+:weeknotes-notes/store
+:weeknotes-notes/injected-app
+:weeknotes.notes/http-server
+
 (defn config-dev []
-  {:storage-path ".local/storage"
-   :port 7984})
+  {:weeknotes-notes/store {:root ".local/storage"}
+   :weeknotes-notes/injected-app {}
+   :weeknotes.notes/http-server {:port 7984}})
 
 (defn config-prod []
   (assert (not (str/blank? (System/getenv "GARDEN_STORAGE"))))
-  {:storage-path (System/getenv "GARDEN_STORAGE")
-   :port 7777})
+  {:weeknotes-notes/store {:root (System/getenv "GARDEN_STORAGE")}
+   :weeknotes-notes/injected-app {}
+   :weeknotes.notes/http-server {:port 7777}})
 
 (defmethod ig/init-key :weeknotes/store
   [_ _])
