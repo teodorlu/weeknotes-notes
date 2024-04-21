09cd61bd12c2b09e792f87273350d4eb01aa527e
Teodor Heggelund <git@teod.eu>
2024-04-21 09:05:47 +0200

    Yay, it works!

--- src/weeknotes_notes/ui.clj
+++ src/weeknotes_notes/ui.clj
@@ -12,15 +12,22 @@
     [:div [:input {:type "submit" :value "Submit note"}]]]
    [:p "All notes are public. Don't write secrets!"]))
 
+(defonce s (atom nil))
+
 (defn fragment-list-notes [req]
   (when-let [store (:weeknotes-notes/store req)]
+    (reset! s store)
     (list
      [:p "Notes + content:"]
      [:ul
       (for [uuid (store/list-uuids store)]
         [:li
          [:p [:strong uuid]]
-         [:p (store/load-one store uuid) ]])])))
+         [:p (:note (store/load-one store uuid)) ]])])))
+
+(let [store @s
+      uuid (first (store/list-uuids store))]
+  (store/load-one store uuid))
 
 (defn page-index [req]
   {:status 200
