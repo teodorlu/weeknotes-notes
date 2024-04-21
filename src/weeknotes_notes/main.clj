fcbd8bba0c6fc35311486bb68694aae3a8574f74
Teodor Heggelund <git@teod.eu>
2024-04-21 15:50:38 +0200

    comment-out unfinished stuff

--- src/weeknotes_notes/main.clj
+++ src/weeknotes_notes/main.clj
@@ -17,6 +17,7 @@
                             ))
     )
   (let [opts (merge {:port 7984
+                     #_#_
                      :storage-dir ""})]))
 
 ;; 1. I can pass additional arguments to clojure -X
