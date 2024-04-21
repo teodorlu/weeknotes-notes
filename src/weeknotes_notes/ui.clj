e972a3beeb17127705dae2aefc2d9395de0e3e41
Teodor Heggelund <git@teod.eu>
2024-04-21 10:27:17 +0200

    refactor

--- src/weeknotes_notes/ui.clj
+++ src/weeknotes_notes/ui.clj
@@ -38,8 +38,9 @@
         [:meta {:charset "UTF-8"}]
         [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
        [:body
-        #_#_
+        #_
         (fragment-write-note req)
+        #_
         (fragment-list-notes req)
         [:p "Source on Github: " [:a {:href path/github-source-url} path/github-shortname] "."]])})
 
