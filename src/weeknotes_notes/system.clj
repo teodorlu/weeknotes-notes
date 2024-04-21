4817b9f14cfcc80fe27e1e55bf476a84414b5a86
Teodor Heggelund <git@teod.eu>
2024-04-21 09:36:22 +0200

    weeknotes config

--- src/weeknotes_notes/system.clj
+++ src/weeknotes_notes/system.clj
@@ -8,6 +8,10 @@
 ;;
 ;; http-server
 
+(defmethod ig/init-key :weeknotes/config
+  [_ _]
+  {:garden-storage-path (or (System/getenv "GARDEN_STORAGE") ".local/garden-storage")
+   :port (if (System/getenv "GARDEN_STORAGE") 7777 7984)})
+
 (defmethod ig/init-key :weeknotes/store
-  []
-  )
+  [_ _])
